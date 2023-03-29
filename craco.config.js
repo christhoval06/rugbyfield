feaa7f698707ada922839a05ee1f4e034d369095
Christhoval Barba <christhoval@mftech.io>
2023-03-28 19:25:08 -0500

    update files

--- craco.config.js
+++ craco.config.js
@@ -1,16 +1,10 @@
 module.exports = {
   babel: {
-    // presets: ["@babel/preset-react"/*"@babel/preset-env"*/],
     plugins: [
       ['@babel/plugin-proposal-decorators', { decoratorsBeforeExport: true }],
       ['@babel/plugin-proposal-class-properties', { loose: true }],
       '@babel/plugin-transform-runtime',
     ],
-    loaderOptions: (babelLoaderOptions, { env, paths }) => {
-      console.log("BABEL");
-      console.log(babelLoaderOptions);
-      return babelLoaderOptions;
-    },
   },
 };
 // https://codepen.io/jzmmm/pen/XKGOJk
